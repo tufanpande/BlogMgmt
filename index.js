3ad92fece40482a5879a5b7eb62e25ca1b84cd3f
Tufan <tufanpande@gmail.com>
2024-02-05 09:42:18 +0545

    Main

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 require("dotenv").config();
-const morgan= require("morgan");
 const express = require("express");
+const mongoose= require("mongoose");
+const morgan= require("morgan");
 
 const indexRouter = require("./routes");
-const mongoose= require("mongoose");
+
 const PORT= Number(process.env.PORT);
+
 const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static("public"));
+
+
 
 mongoose.connect(process.env.DB).then(()=>{
     console.log("Database is connected");
 });
 
-
-app.use(express.static("public"));
-
-app.use(express.json());
-app.use(morgan("dev"));
-
 app.use("/", indexRouter);
+
+
+
+
+
+
 
 app.use((err,req, res,next)=>{
     err= err ? err.toString() :"something went wrong...";
