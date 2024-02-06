f252fd467203e661ff6a819e5ee7e67120784430
Tufan <tufanpande@gmail.com>
2024-02-06 13:30:13 +0545

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 
  const blogRouter=  require("../modules/blogs/blog.route");
+const userRouter=require("../modules/users/user.route");
 
 
 
 
 
 
-
- router.use("/blogs",blogRouter);
+ router.use("/api/v1/blogs",blogRouter);
+ router.use("/api/v1/users",userRouter);
  module.exports = router;
