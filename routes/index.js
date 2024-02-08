3590cfa8c144c769c680c8cf7238a209d333246e
Tufan <tufanpande@gmail.com>
2024-02-08 21:19:12 +0545

    Router Index

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 
  const blogRouter=  require("../modules/blogs/blog.route");
 const userRouter=require("../modules/users/user.route");
+const roleRouter=require("../modules/roles/role.route");
 
 
 
-
-
-
+router.use("/api/v1/roles",roleRouter);
  router.use("/api/v1/blogs",blogRouter);
  router.use("/api/v1/users",userRouter);
+ router.use("api/v1/users/register",userRouter);
  module.exports = router;
